4ebbe33e39641c57477b5af3d98b61588f4e150f
Kenneth Sng <kenneth@open.gov.sg>
2023-07-16 03:00:31 +0800

    Adding process.env.PORT for Heroku deployment

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.post("/stand", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on Port 3000.");
 });
