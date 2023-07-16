bbd17e51915ef8101172654427953aee8c43c2f9
Kenneth Sng <kenneth@open.gov.sg>
2023-07-17 00:47:24 +0800

    Added bet and wallet to the game. We can now calculate total winnings.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let showBankerCards;
 let deck;
 let winner = "";
 let wallet = 50;
-let bet;
+let bet = 0;
 
 // Define Start Game function. I need to refactor this later to push this function to logic.js
 
@@ -33,7 +33,6 @@ const startGame = function () {
   playerCards.push(logic.randomDrawOne(deck));
   playerCards.push(logic.randomDrawOne(deck));
   playerPoints = logic.countPoints(playerCards);
-  console.log(logic.checkNatural(playerCards));
 
   // Draw two cards for the banker
   bankerCards = [];
@@ -41,7 +40,6 @@ const startGame = function () {
   bankerCards.push(logic.randomDrawOne(deck));
   bankerPoints = logic.countPoints(bankerCards);
   showBankerCards = false;
-  console.log(logic.checkNatural(bankerCards));
 };
 
 // Define Settle Game function. I need to refactor this to move it into logic.js.
@@ -55,6 +53,7 @@ const settleGame = function () {
     // Game ends
     console.log("Player busts. Game over!");
     winner = "Banker";
+    wallet -= bet;
   } else if (
     // Scenario 2: Player has a Natural, but Banker doesn't.
     playerPoints === 21 &&
@@ -63,6 +62,7 @@ const settleGame = function () {
   ) {
     console.log("Player has a Natural");
     winner = "Player";
+    wallet += bet * 1.5;
   } else {
     // Dealer reveals his cards and deals more if he doesn't have 17.
 
@@ -80,15 +80,15 @@ const settleGame = function () {
     //Scenario 3: Banker busts.
     if (bankerPoints > 21) {
       winner = "Player";
-
+      wallet += Number(bet);
       // Scenario 4: Banker beats Player.
     } else if (bankerPoints > playerPoints) {
       winner = "Banker";
-
+      wallet -= bet;
       // Scenario 5: Player beats Banker.
     } else if (bankerPoints < playerPoints) {
       winner = "Player";
-
+      wallet += bet;
       // Scenario 6: Draws
     } else {
       winner = "None";
@@ -98,23 +98,46 @@ const settleGame = function () {
   return winner;
 };
 
-// Start game
 startGame();
 
-// Starts when player goes to page
+// Starts when player goes to page. Player has to choose bet amount.
 app.get("/", function (req, res) {
-  // Count player's points. Remove 'Hit' button if player is 21 points.
-  playerPoints = logic.countPoints(playerCards);
+  res.render("home");
+  startGame();
+});
+
+// Starts when player goes to page
+app.post("/game", function (req, res) {
+  // Update bet based on user inputs
+  bet = req.body.bet;
+  console.log(bet);
+  console.log(wallet);
 
   // Show two cards on screen.
-  res.render("game", {
-    playerCards: playerCards,
-    playerPoints: playerPoints,
-    bankerCards: bankerCards,
-    bankerPoints: bankerPoints,
-    showBankerCards: showBankerCards,
-    winner: winner,
-  });
+  res.redirect("/game");
+});
+
+// Starts when player goes to page
+app.get("/game", function (req, res) {
+  // If the user navigates to "/game" without setting betting amount, we will direct user to home page.
+  if (bet === 0) {
+    res.render("home");
+  } else {
+    // Count player's points. Remove 'Hit' button if player is 21 points.
+    playerPoints = logic.countPoints(playerCards);
+
+    // Show two cards on screen.
+    res.render("game", {
+      playerCards: playerCards,
+      playerPoints: playerPoints,
+      bankerCards: bankerCards,
+      bankerPoints: bankerPoints,
+      showBankerCards: showBankerCards,
+      winner: winner,
+      bet: bet,
+      wallet: wallet,
+    });
+  }
 });
 
 app.post("/hit", function (req, res) {
@@ -123,21 +146,20 @@ app.post("/hit", function (req, res) {
   if (playerPoints < 21) {
     playerCards.push(logic.randomDrawOne(deck));
   }
-  res.redirect("/");
+  res.redirect("/game");
 });
 
 app.post("/stay", function (req, res) {
   console.log("Player Stays!");
   settleGame();
-  res.redirect("/");
+  res.redirect("/game");
 });
 
 app.post("/nextgame", function (req, res) {
-  bet = req.body.bet;
-  console.log(req.body.bet);
+  bet = Number(req.body.bet);
   startGame();
   winner = "";
-  res.redirect("/");
+  res.redirect("/game");
 });
 
 app.listen(process.env.PORT || 3000, function () {
