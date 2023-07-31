ffc02f76475099c84d3441f387e5e023549daf0a
Kenneth Sng <kenneth@open.gov.sg>
2023-08-01 01:46:07 +0800

    Remove database.js file to troubleshoot database deployment.

--- data/database.js
+++ data/database.js
@@ -19,60 +19,31 @@ Banker: We will update this each time a player finishes a game (i.e. hits 'Game 
 */
 
 // require("dotenv").config();
-// const { Pool } = require("pg");
+// const { Client } = require("pg");
+// const fs = require("fs");
+// const path = require("path");
+// const caCertPath = path.join(__dirname, "..", "certs", "global-bundle.pem");
+// const caCert = fs.readFileSync(caCertPath);
 
-// const pool = new Pool({
+// const client = new Client({
 //   host: process.env.DB_HOST,
 //   port: process.env.DB_PORT,
 //   user: process.env.DB_USER,
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_DATABASE,
+//   ssl: {
+//     ca: caCert,
+//   },
 // });
 
-// // Test the connection
-// pool.query("SELECT NOW()", (err, res) => {
-//   if (err) {
-//     // Log the detailed error message and other relevant information
-//     console.error("Error connecting to the database:");
-//     console.error("Host:", process.env.DB_HOST);
-//     console.error("Port:", process.env.DB_PORT);
-//     console.error("User:", process.env.DB_USER);
-//     console.error("Database:", process.env.DB_DATABASE);
-//     console.error("Error message:", err.message);
-//     console.error("Stack trace:", err.stack);
-//     // If you are using pg-promise, you can log the query being executed:
-//     // console.error('Query:', err.query);
+// client.connect();
+
+// client.query("Select * from GAMES", (err, res) => {
+//   if (!err) {
+//     console.log(res.rows);
 //   } else {
-//     console.log("Connected to the database at:", process.env.DB_HOST);
+//     console.log(err.message);
 //   }
 // });
 
-require("dotenv").config();
-const { Client } = require("pg");
-const fs = require("fs");
-const path = require("path");
-const caCertPath = path.join(__dirname, "..", "certs", "global-bundle.pem");
-const caCert = fs.readFileSync(caCertPath);
-
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    ca: caCert,
-  },
-});
-
-client.connect();
-
-client.query("Select * from GAMES", (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err.message);
-  }
-});
-
-module.exports = client;
+// module.exports = client;
